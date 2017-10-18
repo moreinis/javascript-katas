8a810127c3369c1bfba9f82de559087adc542965
moreinis <31930502+moreinis@users.noreply.github.com>
2017-10-17 20:42:39 -0400

    Instructions Only

---
+++ 1-conditionals.js
@@ -0,0 +1,8 @@
+/* Rock Paper Scissors Kata */
+
+// Prompt for rock, paper or scissors
+// When computer receives the response, reply and evaluate the result win, lose, draw.
+
+//1. Cheater: Computer always states the winning countermove and says "I win!" after.
+//2. Fair Play Array: Computer picks a random countermove, then evaluates with a switch statement.
+//3. With Retake: If both enter the same value, ask again (while loop).
