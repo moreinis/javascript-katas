71af23770c1ce9db24a8c37f85e355117872b4c1
moreinis <31930502+moreinis@users.noreply.github.com>
2017-10-18 12:04:59 -0400

    Includes function and switch

--- 1-conditionals.js
+++ 1-conditionals.js
@@ -3,6 +3,8 @@
 // Prompt for rock, paper or scissors
 // When computer receives the response, reply and evaluate the result win, lose, draw.
 
-//1. Cheater: Computer always states the winning countermove and says "I win!" after.
-//2. Fair Play Array: Computer picks a random countermove, then evaluates with a switch statement.
-//3. With Retake: If both enter the same value, ask again (while loop).
+//1. Cheater: Computer always states the winning countermove and says "I win!" after. Replay while choice is not something.
+//2. Recode using a switch command
+//3. Record using a function with a switch
+//4. Fair Play Array: Computer picks a random countermove, then evaluates with a switch statement.
+//5. With Retake: If both enter the same value, ask again (while loop).
