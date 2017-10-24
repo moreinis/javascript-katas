ce31809939f7cc1afa81b257656e0fcd0b03091b
moreinis <31930502+moreinis@users.noreply.github.com>
2017-10-24 16:56:11 -0400

    All working!

--- rps-functions.js
+++ rps-functions.js
@@ -1,13 +1,28 @@
 // initialize variables
-var hands=["rock","paper","scissors","stop"];
+var hands=["rock","paper","scissors","stop"]; // array of values
+var cpuNum=0;
+var userNum=0;
+var winner="";
+var index=0;
+var goodHand=false;
+var userHand="";
+
+// run program
 do {
-	cpuNum=Math.round(Math.random()*2);
-	userNum=getUserHand(hands):
-	winner=evaluateHand(hands,cpuNum,userNum);
-	alert("You picked "+hands[userNum]+". CPU picks "+hands[cpuNum]+". Therefore, "+winner+" wins!"
-while (userHand!="stop");
+	cpuNum=Math.round(Math.random()*2); // computer picks 0-2
+	userNum=getUserHand(hands);// user picks 0-2
+	winner=evaluateHand(hands,cpuNum,userNum); // compare hands, like 01,20
+	if (winner=="draw") {
+		alert("Draw. Again!");
+		}
+	else if (winner=="you" || winner=="I"){
+		alert("You picked "+hands[userNum]+". I picked "+hands[cpuNum]+" so "+winner+" won!");
+		}
+}
+while (winner!="stop"); // until user enters stop.
+alert("Thank you for playing!");
 
-
+// get user input function
 function getUserHand(hands) {
 	goodHand=false;
 	do {
@@ -18,49 +33,44 @@ function getUserHand(hands) {
 				userNum=index;
 			}
 		}
-		while (goodHand==false);
+		if (goodHand===false) {
+			alert("invalid entry");
+		}
 	}
+	while (goodHand===false);
 	return userNum;
 }
 
-function evaluateHand(hands,userNum,cpuNum,){
-	var winner="cpu";
-	if (userHand==cpuHand){
-		alert("Draw!");
-		winner="none";
+// evaluate winner function
+function evaluateHand(hands,cpuNum,userNum){
+	if (userNum==cpuNum){
+		winner="draw";
 	}
 	else if (hands[userNum]=="stop"){
-		alert("Thank you for playing!");
-		winner="none";
+		winner="stop";
 	}
-	else switch (userHand+cpuHand) {
-			case 01:{winner="human";}
+	else {
+		var hand=cpuNum.toString()+userNum.toString();
+		switch (hand) {
+			case "01":
+				winner="you";
 				break;
-			case 02:
-				{
-					winner="cpu";
-				}
+			case "02":
+				winner="I";
 				break;
-			case 10:
-				{
-					winner="cpu";
-				}
+			case "10":
+				winner="I";
 				break;
-			case 12:
-				{
-					winner="human";
-				}
+			case "12":
+				winner="you";
 				break;
-			case 20:
-				{
-					winner="cpu";
-				}
+			case "20":
+				winner="you";
 				break;
-			case 21:
-				{
-					winner="human";
-				}
+			case "21":
+				winner="I";
 				break;
 		}
+	}
 	return winner;
 }
